00e3c0747d5b71118c2d0747b46ee3f45bb25a2c
kingoftheHIU <hsy1169@naver.com>
2024-06-07 15:38:29 +0900

    Update my_1st_sl.py

--- my_1st_sl.py
+++ my_1st_sl.py
@@ -4,6 +4,9 @@ st.title('「 ✦ SUYEON HONG ✦ 」')
 
 st.write(' ➡ 본 웹페이지는 홍수연의 비전과 앞으로의 계획에 대해 소개하는 페이지 입니다.')
 
+st.write('')
+st.write('')
+
 st.header('✔️사이드바')
 st.sidebar.write('## 사이드바 텍스트')
 st.sidebar.checkbox('체크박스 1')
@@ -11,23 +14,23 @@ st.sidebar.checkbox('체크박스 2')
 st.sidebar.radio('라디오 버튼', ['radio 1', 'radio 2', 'radio 3'])
 st.sidebar.selectbox('셀렉트박스', ['select 1', 'select 2', 'select 3'])
 
-st.header('## 1️⃣ 개인정보')
+st.header('1️⃣ 개인정보')
 col_1, col_2, col_3 = st.columns([1,1,1])
 
 with col_1 :
     st.write('### 학적사항')
-    st.write('3학년')
-    st.write('22학년도 입학')
+    st.write('- 3학년')
+    st.write('- 22학년도 입학')
     
 with col_2 :
     st.write('### 전공')
-    st.write('- 주전공 : 경영학과')
-    st.write('- 복수전공 : 산업데이터공학과')
+    st.write('- **주전공 : 경영학과**')
+    st.write('- **복수전공 : 산업데이터공학과**')
 
 with col_3 : 
     st.write('### 교내활동')
-    st.write('중앙동아리 터사랑 부회장')
-    st.write('총동아리연합회 레저분과장')
+    st.write('- 중앙동아리 터사랑 부회장')
+    st.write('- 총동아리연합회 레저분과장')
     
 
 st.markdown(
