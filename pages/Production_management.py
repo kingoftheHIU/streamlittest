c7dcc309abddb328f2b3dbad0bb649c96e9a974f
kingoftheHIU <hsy1169@naver.com>
2024-06-20 01:36:21 +0900

    Update Production_management.py

--- pages/Production_management.py
+++ pages/Production_management.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title('⚙️생산경영 (Production Magagement)🔧')
-st.subtitle('➡ Optimizing Processes, Enhancing Efficiency')
+st.subheader('➡ Optimizing Processes, Enhancing Efficiency')
 st.write('')
 st.write('')
 
