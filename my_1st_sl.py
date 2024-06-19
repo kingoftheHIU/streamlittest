6291d3111d4bcd4d03f2629ff11427b6e15ffbbb
kingoftheHIU <hsy1169@naver.com>
2024-06-19 21:55:43 +0900

    Update my_1st_sl.py

--- my_1st_sl.py
+++ my_1st_sl.py
@@ -63,12 +63,12 @@ with tab_3:
 with tab_4:
     st.write('### 취미와 관심사(Hobbies and Interests)')
     col1, col2, col3 = st.columns(['Cat', 'Dog', 'Owl'])
-
-with col1:
-    st.text('Cat')
     
-with col2:
-    st.text('Dog')
-    
-with col3:
-    st.text('Owl')
+    with col1:
+        st.text('Cat')
+        
+    with col2:
+        st.text('Dog')
+        
+    with col3:
+        st.text('Owl')
