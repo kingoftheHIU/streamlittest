d3fbafeec5af4b734cf69da88a5c5439c48c3c9a
kingoftheHIU <hsy1169@naver.com>
2024-06-19 13:09:17 +0900

    Update and rename my_1st_sl.py to Main_page.py

--- Main_page.py
+++ Main_page.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from PIL import Image
+aaa=Image.open('C:\Users\hsy11\Downloads\free-icon-woman-6785697.png')
 
 st.title('「 ✦ SUYEON HONG ✦ 」')
 st.subheader(' ➡ Welcome to personal page of Suyeon! ')
@@ -11,8 +13,9 @@ col_1, col_2= st.columns([1,1])
 
 with col_1 :
     st.write('### 학적사항')
-    st.image('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.com%2Fkr%2Ffree-icon%2Fwoman_3884857&psig=AOvVaw0-wxwZ1uD59sINSgt8Ezhs&ust=1718855837132000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCID02ZLj5oYDFQAAAAAdAAAAABAE')
+    st.image(aaa)
     
+   
 with col_2 :
     st.write('### 전공')
     st.write('- **주전공 : 경영학과**')
