c6b1ff819ed6bd55574501e611a5c8082b6090dc
kingoftheHIU <hsy1169@naver.com>
2024-06-19 19:10:59 +0900

    Update my_1st_sl.py

--- my_1st_sl.py
+++ my_1st_sl.py
@@ -27,3 +27,21 @@ st.markdown(
     - **주소(Adress)** : 경기도 광명시 하안동
    '''
    )
+st.header('3️⃣ About Me')
+tab_1, tab_2, tab_3, tab_4 = st.tabs(['학력(Educational Background)', '경력(Professional Background)', '기술(Skills)', '취미와 관심사(Hobbies and Interests)'])
+
+with tab_1:
+    st.write('## 학력(Educational Background)')
+    st.write('이것은 탭A의 내용입니다.')
+
+with tab_2:
+    st.write('## 경력(Professional Background)')
+    st.write('이것은 탭B의 내용입니다.')
+    
+with tab_3:
+    st.write('## 기술(Skills)')
+    st.write('이것은 탭A의 내용입니다.')
+
+with tab_4:
+    st.write('## 취미와 관심사(Hobbies and Interests)')
+    st.write('이것은 탭B의 내용입니다.')
