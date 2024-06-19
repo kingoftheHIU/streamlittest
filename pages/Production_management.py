998f08c7941c63efb099dcbd9119cc870f833688
kingoftheHIU <hsy1169@naver.com>
2024-06-20 00:54:22 +0900

    Update Production_management.py

--- pages/Production_management.py
+++ pages/Production_management.py
@@ -1,7 +1,8 @@
 import streamlit as st
-import pandas as pd
 
 st.title('⚙️생산경영 (Production Magagement)🔧')
 st.subtitle('➡ Optimizing Processes, Enhancing Efficiency')
 st.write('')
 st.write('')
+
+st.header('')
