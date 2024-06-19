8b34c34ba8cc751ba53b8200e73e59f4520f9cbf
kingoftheHIU <hsy1169@naver.com>
2024-06-19 13:38:00 +0900

    Update my_1st_sl.py

--- my_1st_sl.py
+++ my_1st_sl.py
@@ -11,10 +11,9 @@ st.header('1️⃣ Introduction')
 col_1, col_2= st.columns([1,1])
 
 with col_1 :
-    st.write('### 학적사항')
     st.image('girl.png')
    
 with col_2 :
     st.write('### 전공')
-    st.write('- **주전공 : 경영학과**')
-    st.write('- **복수전공 : 산업데이터공학과**')
+    st.write('안녕하세요! 😊 저는 홍수연(Suyeon Hong)입니다. 항상 웃음을 잃지 않고 주변에 긍정 에너지를 전파하는 것이 제 특기랍니다! 🌟 새로운 도전과 배움을 사랑하며, 사람들과의 소통을 즐기는 외향적인 성격을 가지고 있어요. 생산경영에 대한 열정으로 매일 조금씩 성장해 나가는 중이에요. ')
+
